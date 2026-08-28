ae1f5b6e2f40d63be4834cfdf02e5f285ebd75ec
Amzadz-stack <a.zameer1985@gmail.com>
2026-08-28 23:34:48 +0530

    Update stg_raw_trades.sql

--- dbt_project/models/staging/stg_raw_trades.sql
+++ dbt_project/models/staging/stg_raw_trades.sql
@@ -1,1 +1,19 @@
+{{ config(materialized='view') }}
 
+SELECT
+    event_id,
+    trade_ref,
+    version,
+    instrument_type,
+    notional,
+    currency,
+    trade_date,
+    maturity_date,
+    counterparty,
+    trader_id,
+    source_system,
+    raw_payload,
+    ingested_at,
+    CURRENT_DATE() AS processing_date
+FROM {{ source('raw', 'trade_events') }}
+WHERE is_processed = FALSE

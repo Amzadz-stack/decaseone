65d8659f80c867d54e6f45a05957f950ef47826d
Amzadz-stack <a.zameer1985@gmail.com>
2026-08-28 23:37:40 +0530

    Update rejected_trades.sql

--- dbt_project/models/ marts/rejected_trades.sql
+++ dbt_project/models/ marts/rejected_trades.sql
@@ -1,1 +1,24 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
 
+SELECT
+    UUID_STRING() AS rejection_id,
+    event_id,
+    trade_ref,
+    incoming_version AS version,
+    instrument_type,
+    notional,
+    currency,
+    trade_date,
+    maturity_date,
+    counterparty,
+    trader_id,
+    processing_action AS rejection_code,
+    rejection_reason,
+    raw_payload,
+    CURRENT_TIMESTAMP() AS rejected_at
+FROM {{ ref('int_trade_classification') }}
+WHERE processing_action LIKE 'REJECT%'
